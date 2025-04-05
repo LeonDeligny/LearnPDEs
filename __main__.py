153fb248bdc515e632a2b40a36abecb6b984e7e2
Deligny <leon.deligny@icloud.com>
2025-04-05 20:33:10 +0200

    flake8 .

--- __main__.py
+++ __main__.py
@@ -23,13 +23,8 @@ Scenarios:
 import torch
 import numpy as np
 
-from torch import Tensor
 from model.pinn import PINN
 from model.trainer import Trainer
-from typing import (
-    Dict,
-    Callable,
-)
 # from functools import partial
 
 from utils.decorators import time
@@ -45,26 +40,7 @@ from __init__ import device
 
 
 @time
-def main(
-    input_space: Tensor = torch.cat([
-        linspace(-3, 3, 10_000).view(-1, 1),
-        torch.tensor([0.0]).view(-1, 1),
-    ]).unique(dim=0).sort(dim=0).values,
-    nn_params: Dict = {
-        'hidden_dim': 100,
-        'num_hidden_layers': 4,
-    },
-    training_params: Dict = {
-        'learning_rate': 0.001,
-        'nb_epochs': 10_000,
-    },
-    loss_func_name: str = 'cosinus_loss',
-    input_homeo: Callable[[Tensor], Tensor] = identity,
-    output_homeo: Callable[[Tensor], Tensor] = identity,
-    # partial(fourier, dim=10, scale=1.0),
-    encoding: Callable[[Tensor], Tensor] = identity,
-    analytical: Callable[[float], float] = np.cos,
-) -> None:
+def main() -> None:
     '''
     Description of workflow.
         1. Construct model.
@@ -74,19 +50,28 @@ def main(
 
     # 1. Construct model
     pinn = PINN(
-        nn_params=nn_params,
-        input_homeo=input_homeo,
-        output_homeo=output_homeo,
-        encoding=encoding,
+        nn_params={
+            'hidden_dim': 100,
+            'num_hidden_layers': 4,
+        },
+        input_homeo=identity,
+        output_homeo=identity,
+        encoding=identity,
     ).to(device)
 
     # 2. Train model
     trainer = Trainer(
         pinn=pinn,
-        input_space=input_space,
-        training_params=training_params,
-        loss_func_name=loss_func_name,
-        analytical=analytical,
+        input_space=torch.cat([
+            linspace(-3, 3, 10_000).view(-1, 1),
+            torch.tensor([0.0]).view(-1, 1),
+        ]).unique(dim=0).sort(dim=0).values,
+        training_params={
+            'learning_rate': 0.001,
+            'nb_epochs': 10_000,
+        },
+        loss_func_name='cosinus_loss',
+        analytical=np.cos,
     )
 
     trainer.train()
